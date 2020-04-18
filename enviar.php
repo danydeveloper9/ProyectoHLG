6bddd8aac3fec9aaa0b90816e364e3726c080d1d
dev-danyWEB <59677234+dev-danyWEB@users.noreply.github.com>
2020-04-18 14:37:13 -0500

    FormularioTerminado

---
+++ enviar.php
@@ -0,0 +1,34 @@
+<?php
+
+    function validar_campo($campo){
+        $campo = trim($campo);
+        $campo = stripcslashes($campo);
+        $campo = htmlspecialchars($campo);
+
+        return $campo;
+    }
+
+    header('Content-type: application/json');
+
+    if(isset($_POST["nombre"]) && !empty($_POST["nombre"]) &&
+       isset($_POST["correo"]) && !empty($_POST["correo"]) &&
+       isset($_POST["mensaje"]) && !empty($_POST["mensaje"])){
+
+        $destinoMail = "gdanielmh35@gmail.com";
+        $nombre = validar_campo($_POST["nombre"]);
+        if(isset($_POST["telefono"])){
+            $telefono = validar_campo($_POST["telefono"]);
+        }
+        $correo = validar_campo($_POST["correo"]);
+        $mensaje = validar_campo($_POST["mensaje"]);
+
+        $contenido = "Nombre: " . $nombre . "\n Teléfono: " . $telefono;
+        $contenido .= "\n Correo: " . $correo;
+        $contenido .= "\n Mensaje: " . $mensaje;
+       
+        mail($destinoMail, "Mensaje de contacto del cliente" . $nombre, $contenido);
+
+        return print(json_encode('ok'));
+    }
+
+    return print(json_encode('no'));
--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
-$(function(){
+$(document).ready(function(){
     
     /* 1. Funciones Menu Principal */
     /* Insertar y quitar ".icono-cerrar" al boton del menu */
@@ -39,4 +39,42 @@ $(function(){
         highlightClass: 'active'
     });
 
-})
+    /* 5. AJAX FORMULARIO DE CONTACTO*/
+    $(".formulario-contacto").bind("submit", function(){
+        $("#alerta").slideDown();
+        $(".close").click(function(){
+            $("#alerta").slideUp();
+        });
+
+        $.ajax({
+            type: $(this).attr("method"),
+            url: $(this).attr("action"),
+            data: $(this).serialize(),
+            success: function(respuesta){
+                if(respuesta == "ok"){
+                    $("#alerta").removeClass("hide").removeClass("alert-danger").removeClass("alert-success").removeClass("show").addClass("show").addClass("alert-success");
+                    $("#btn-alerta").removeClass("d-none").addClass("d-block");
+                    $(".respuesta").html("Enviado!");
+                    $(".mensaje-alerta").html("El mensaje ha sido enviado correctamente.");
+                    $("#limpia").trigger("click");
+                }
+                else{
+                    $("#alerta").removeClass("hide").removeClass("alert-danger").removeClass("alert-success").removeClass("show").addClass("show").addClass("alert-danger");
+                    $("#btn-alerta").removeClass("d-none").addClass("d-block");
+                    $(".respuesta").html("Error al enviar!");
+                    $(".mensaje-alerta").html("No se pudo enviar tu mensaje, intentelo de nuevo.");
+                }
+            },
+            error: function(){
+                $("#alerta").removeClass("hide").removeClass("alert-danger").removeClass("alert-success").removeClass("show").addClass("show").addClass("alert-danger");
+                $("#btn-alerta").removeClass("d-none").addClass("d-block");
+                $(".respuesta").html("Error al enviar!");
+                $(".mensaje-alerta").html("No se pudo enviar tu mensaje, intentelo de nuevo.");
+            }
+        });
+
+        return false;
+
+    });
+
+});
